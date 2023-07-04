2cdcd934f103873363e6876dc49e790587be4082
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-07-04 14:01:57 +0300

    Added tailwind framework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Home from "./components/Homepage/Home.jsx"
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar/>
+      <Routes>
+          <Route exact path="/" element={<Home/>} />
+      </Routes>
     </div>
   );
 }
