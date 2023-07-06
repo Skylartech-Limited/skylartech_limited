b71855bf143e35564e0a0d18ffbb2365980c378b
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-07-06 16:55:13 +0300

    Featuring:     1. Added font-awesome icon script     2. Added routes to various pages     3. Added NAvbar items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Home from "./components/Homepage/Home.jsx"
+import Services from './components/Services/Services';
+import Contact from './components/Contact/Contact';
+import Technologies from './components/Technologies/Technologies';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Navbar/>
       <Routes>
           <Route exact path="/" element={<Home/>} />
+          <Route exact path='/technologies' element={<Technologies/>} />
+          <Route exact path='/contact-us' element={<Contact/>} />
+          <Route exact path='/services' element={<Services/>} />
+          <Route exact path='/portfolio' element={<Portfolio/>} />
       </Routes>
     </div>
   );
