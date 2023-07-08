a8ae6d4e138c4e0ced133b69db756e4f35b96393
nicolenjoroge <nixiepatwelve@gmail.com>
2023-07-08 17:22:08 +0300

    First container home page v1

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-left p-10'>
+      <>
+      <h2 className='text-2xl font-medium text-gray-800 py-2 pt-10'><span className='underline decoration-3 decoration-amber-500 underline-offset-8 '>Del</span>ivering quality projects <span className='text-amber-600'>beyond expectations</span></h2>
+      <h1 className='max-w-xl text-4xl py-2 font-semibold text-gray-800'>Digitize Your Business With Our Modern Development Solutions</h1>
+      <p className='text-lg max-w-xl py-5 leading-8 text-gray-600'>Devinx is a specialized mobile and web app development company with a focus on smart technologies and customer satisfaction. Born in 2018, our company has been delivering high-end quality solutions to global businesses with a success rate of 100%. Hire our expertise at affordable prices to have the right tech solutions for your company.
+
+      </p>
+      <ul>
+        <li>
+          <button className="transition ease-in delay-75 hover:-translate-y-0.5 hover:scale-110 border-2 border-amber-500 bg-white hover:bg-amber-500 text-black px-4 py-2 rounded ">Let's Talk</button>
+        </li>
+      </ul>
+      
+      </>
+    </div>
   )
 }
 
