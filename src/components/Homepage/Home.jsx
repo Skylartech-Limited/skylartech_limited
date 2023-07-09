50b964dbded84be787c42478482fc1bfcc7a493d
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-07-09 13:27:43 +0300

    Improved on responsiveness

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="text-left p-10">
       <>
-        <h2 className="text-2xl font-medium text-gray-800 py-2 pt-10">
+        <h2 className="text-2xl md:text-2xl lg:text-3xl xl:text-2xl font-medium text-gray-800 py-2 pt-10">
           Delivering quality projects{" "}
           <span className="text-amber-500 font-medium">
             <Typewriter
@@ -21,10 +21,10 @@ const Home = () => {
             <Cursor cursorStyle="~" />
           </span>
         </h2>
-        <h1 className="max-w-xl text-4xl py-2 font-semibold text-gray-800">
+        <h1 className="max-w-xl text-3xl md:text-4xl lg:text-5xl xl:text-4xl py-2 font-semibold text-gray-800">
           Digitize Your Business With Our Modern Development Solutions
         </h1>
-        <p className="text-lg max-w-xl py-5 leading-8 text-gray-600">
+        <p className="text-base md:text-lg lg:text-xl xl:text-lg max-w-xl py-5 text-gray-600">
           Skylartech is a specialized mobile and web app development company with a
           focus on smart technologies and customer satisfaction. Born in 2018,
           our company has been delivering high-end quality solutions to global
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -110,7 +110,31 @@ const Navbar = () => {
                 </svg>
               </button>
               {isMobileMenuOpen && (
-                <div className="absolute top-16 right-0 bg-white rounded-md shadow-md mt-2 mr-2">
+                <div
+                  className="absolute top-16 right-0 bg-white rounded-md shadow-md mt-2 mr-2"
+                  style={{
+                    "@media (min-width: 640px)": {
+                      top: "8rem",
+                      right: 0,
+                    },
+                    "@media (min-width: 768px)": {
+                      top: "6rem",
+                      right: 0,
+                    },
+                    "@media (min-width: 1024px)": {
+                      top: "5rem",
+                      right: 0,
+                    },
+                    "@media (min-width: 1280px)": {
+                      top: "4rem",
+                      right: "2rem",
+                    },
+                    "@media (min-width: 1536px)": {
+                      top: "4rem",
+                      right: "4rem",
+                    },
+                  }}
+                >
                   <Link
                     to="/"
                     className="block text-amber-500 hover:text-amber-500 px-4 py-2 font-medium transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110"
