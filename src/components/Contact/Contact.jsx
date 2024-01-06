4264f663d60f7745f4ed4eac904fe0da59aa39a7
Keith Kiama <keith2003@gmail.com>
2024-01-07 01:15:09 +0300

    Added Location div

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Cursor, Typewriter } from "react-simple-typewriter";
-
+import { motion } from "framer-motion";
+import City from "../../assets/Contact Images/City.jpeg";
 const Contact = () => {
   return (
     <>
@@ -109,14 +110,48 @@ const Contact = () => {
           </div>
 
           {/* Submit button */}
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          <motion.button
+            className="bg-slate-800 hover:bg-amber-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
+            whileHover={{ scale: 0.9 }}
+            whileTap={{ scale: 0.8 }}
           >
-            Send Message
-          </button>
+            SUBMIT
+          </motion.button>
         </form>
       </div>
+
+      {/* location */}
+      <div
+  className="flex items-center justify-center min-h-1/2-screen relative" // Adjusted min-height
+  style={{
+    backgroundImage: `url(${City})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  }}
+>
+  <div
+    className="absolute inset-0 bg-black opacity-80"
+    style={{ zIndex: 1 }}
+  ></div>
+  <div className="py-4 md:py-8 lg:py-16 text-center relative z-10"> {/* Adjusted padding */}
+    <div className="flex flex-col items-center space-y-4">
+      {/* Center content vertically */}
+      <div className="rounded-full bg-gray-600 p-9 h-25 w-25"> {/* Adjusted size */}
+        <i className="fa-solid fa-location-dot inline-block text-amber-500 text-xl md:text-2xl lg:text-5xl"></i>
+      </div>
+
+      <h1 className="text-white font-serif text-lg md:text-xl lg:text-2xl py-2"> {/* Adjusted font size */}
+        Location
+      </h1>
+      <h2 className="text-white text-md font-serif">Kenya</h2> {/* Adjusted font size */}
+      <p className="text-amber-500 text-md font-serif">The Water Front, Karen</p> {/* Adjusted font size */}
+      <p className="text-white text-md font-serif">+254 718-257-293</p> {/* Adjusted font size */}
+      <p className="text-white text-md font-serif">info@skylartech.co.ke</p> {/* Adjusted font size */}
+    </div>
+  </div>
+</div>
+
     </>
   );
 };
