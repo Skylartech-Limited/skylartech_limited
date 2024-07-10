71b5c37ff100feaae11ebeec79ec86bee40cf919
Keith Kiama <keithkiama2003@gmail.com>
2024-07-10 10:59:55 +0300

    Featuring: 1. Updated livechat script loading

--- src/components/Livechat/Livechat.jsx
+++ src/components/Livechat/Livechat.jsx
@@ -2,16 +2,22 @@ import React, { useEffect } from 'react';
 
 const LiveChat = () => {
   useEffect(() => {
-    // Create a new script element for Tawk.to integration
     const script = document.createElement('script');
     script.async = true;
     script.src = 'https://embed.tawk.to/65b38aae8d261e1b5f58305c/1hl2lbf78';
     script.charset = 'UTF-8';
     script.setAttribute('crossorigin', '*');
+    script.onload = () => {
+      console.log('Script loaded successfully!');
+    };
+    script.onerror = () => {
+      console.error('Error loading script!');
+    };
     document.head.appendChild(script);
-
+  
     return () => {
-      document.head.removeChild(script); // Cleanup function for script removal
+      // Remove the script removal line to see if it makes a difference
+      // document.head.removeChild(script);
     };
   }, []);
 
